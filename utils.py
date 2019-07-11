ed611fbb6a2d0eb1dcb7f06099acb85bd5906730
seoilgun <seoilgun@gmail.com>
2019-07-11 14:12:20 +0900

    changed wave library to librosa

--- utils.py
+++ utils.py
@@ -13,6 +13,7 @@ def get_mask_from_lengths(lengths):
 
 def load_wav_to_torch(full_path, sampling_rate):
     data = librosa.core.load(full_path, sr=sampling_rate)[0]
+    data = data / np.abs(data).max() * 0.999
     return torch.FloatTensor(data.astype(np.float32))
     # sampling_rate, data = read(full_path)
     # return torch.FloatTensor(data.astype(np.float32)), sampling_rate
