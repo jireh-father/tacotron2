48b41cd3b053d406ba7937cbc30267acb91fb9f6
seoilgun <seoilgun@gmail.com>
2019-07-30 16:14:37 +0900

    changed wave library to librosa

--- web_app.py
+++ web_app.py
@@ -95,8 +95,18 @@ def simple_synth():
                            synth_wav_path=filename,
                            sigma=sigma, sampling_rate=sampling_rate, denoiser_strength=denoiser_strength)
 
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
-#
 @app.route('/download/<path:filename>', methods=['GET', 'POST'])
 def download(filename):
     return send_from_directory(directory=SYNTH_DIR, filename=filename, as_attachment=True)
