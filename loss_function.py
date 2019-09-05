d2329c178a9c0e7e6aa6a289860c372933caea7b
seoilgun <seoilgun@gmail.com>
2019-09-06 07:40:43 +0900

    changed wave library to librosa

--- loss_function.py
+++ loss_function.py
@@ -1,9 +1,11 @@
 from torch import nn
-
+import torch
 
 class Tacotron2Loss(nn.Module):
-    def __init__(self):
+    def __init__(self, use_linear_loss, reg_weight):
         super(Tacotron2Loss, self).__init__()
+        self.use_linear_loss = use_linear_loss
+        self.reg_weight = reg_weight
 
     def forward(self, model_output, targets):
         mel_target, gate_target = targets[0], targets[1]
@@ -15,5 +17,10 @@ class Tacotron2Loss(nn.Module):
         gate_out = gate_out.view(-1, 1)
         mel_loss = nn.MSELoss()(mel_out, mel_target) + \
             nn.MSELoss()(mel_out_postnet, mel_target)
+
         gate_loss = nn.BCEWithLogitsLoss()(gate_out, gate_target)
-        return mel_loss + gate_loss
+        if self.use_linear_loss:
+            linear_loss = torch.mean(torch.abs(mel_out_postnet - mel_target))
+            return mel_loss + gate_loss + linear_loss
+        else:
+            return mel_loss + gate_loss
