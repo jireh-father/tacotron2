1ef548ffc005fb4e5d0ddb419381bf71eb6247a6
seoilgun <seoilgun@gmail.com>
2019-09-06 07:54:15 +0900

    changed wave library to librosa

--- loss_function.py
+++ loss_function.py
@@ -2,10 +2,15 @@ from torch import nn
 import torch
 
 class Tacotron2Loss(nn.Module):
-    def __init__(self, use_linear_loss, reg_weight):
+    def __init__(self, use_linear_loss, use_loss_reduction_sum):
         super(Tacotron2Loss, self).__init__()
         self.use_linear_loss = use_linear_loss
-        self.reg_weight = reg_weight
+        reduction= 'mean'
+        if use_loss_reduction_sum:
+            reduction="sum"
+        self.mse_loss = nn.MSELoss(reduction=reduction)
+        self.bce_loss = nn.BCEWithLogitsLoss(reduction=reduction)
+        self.linear_loss = nn.L1Loss(reduction=reduction)
 
     def forward(self, model_output, targets):
         mel_target, gate_target = targets[0], targets[1]
@@ -15,12 +20,11 @@ class Tacotron2Loss(nn.Module):
 
         mel_out, mel_out_postnet, gate_out, _ = model_output
         gate_out = gate_out.view(-1, 1)
-        mel_loss = nn.MSELoss()(mel_out, mel_target) + \
-            nn.MSELoss()(mel_out_postnet, mel_target)
+        mel_loss = self.mse_loss(mel_out, mel_target) + self.mse_loss(mel_out_postnet, mel_target)
 
-        gate_loss = nn.BCEWithLogitsLoss()(gate_out, gate_target)
+        gate_loss = self.bce_loss(gate_out, gate_target)
         if self.use_linear_loss:
-            linear_loss = torch.mean(torch.abs(mel_out_postnet - mel_target))
+            linear_loss = self.linear_loss(mel_out, mel_target)
             return mel_loss + gate_loss + linear_loss
         else:
             return mel_loss + gate_loss
